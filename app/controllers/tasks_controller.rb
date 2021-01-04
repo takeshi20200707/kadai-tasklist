c3913bc905fa31fee52db5a984e2780e0b770913
Fumiaki Takeshima <takeshima14fumiaki@gmail.com>
2021-01-04 11:41:43 +0000

    ログイン認証と一対多をタスク管理アプリに追加する

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit, :update, :destroy]
     before_action :require_user_logged_in, only: [:index, :show, :new, :edit, :destroy]
+    before_action :correct_user, only: [:show, :update, :destroy]
     
     def index
         @tasks = Task.all
@@ -10,12 +11,13 @@ class TasksController < ApplicationController
     end
 
     def new
-        @task = Task.new
+        if logged_in?
+          @task = current_user.tasks.build
+        end
     end
 
     def create
-        @task = Task.new(task_params)
-        
+        @task = current_user.tasks.build(task_params)
         if @task.save
             flash[:success] = "Taskが正常に投稿されました"
             redirect_to @task
@@ -53,6 +55,13 @@ class TasksController < ApplicationController
     
     def task_params
         params.require(:task).permit(:content, :status)
+    end
+    
+    def correct_user
+      @task = current_user.tasks.find_by(id: params[:id])
+      unless @task
+        redirect_to root_url
+      end
     end
     
 end
